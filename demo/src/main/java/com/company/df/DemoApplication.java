fe783bb56236385341d380972c65a7ecaf883e2c
Seongwan Son <sonson1023@gmail.com>
2019-06-01 23:07:35 +0200

    jasyptStringEncryptor 클래스 mit andere Classes

--- demo/src/main/java/com/company/df/DemoApplication.java
+++ demo/src/main/java/com/company/df/DemoApplication.java
@@ -1,16 +1,36 @@
 package com.company.df;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
-
-public class DemoApplication {
+//따로 하는게 맞지만.
+//암호화 떄문에 CommandLineRunner 추가.
+public class DemoApplication implements CommandLineRunner 
+ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+        System.out.println("=========== Server Start ===========");
 	}
 
+	
+	@Override
+	   public void run(String... args) throws Exception {
+			StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+			pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+			pbeEnc.setPassword("test"); //2번 설정의 암호화 키를 입력
+			
+			String enc = pbeEnc.encrypt("1234"); //암호화 할 내용
+			System.out.println("enc = " + enc); //암호화 한 내용을 출력
+			
+			//테스트용 복호화
+			String des = pbeEnc.decrypt(enc);
+			System.out.println("des = " + des);
+	   }
 }
